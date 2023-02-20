c9bb4254eaa7d690f0412837156b5bfa6ad0f1da
Igor Gouveia <igor.roichman@gmail.com>
2023-02-20 10:42:23 -0300

    deleting api scenario

--- cypress/support/api/adicionar_carrinho.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add('addCarrinho', () => {
-    cy.request({
-        method: 'GET',
-        url: '/carrinho/valor/?envio_id=12015&envio_code=retirar_pessoalmente&valor_frete=0&valor_subtotal=80'
-    }).then( (res) =>{
-        expect(res).to.eq(200)
-    })
-  })
